e86583952b5eb49d562ce7b6d74c8810e2b35739
ESSARHTHUMAR <essarthumar177@gmail.com>
2024-11-18 11:28:01 +0530

    Private routes for Customer & Admin

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,27 +1,40 @@
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Customer from './customer/Customer'
-import Admin from "./admin/Admin"
-import Navbar from "./components/Navbar"
-import Home from "./components/Home"
-import Login from './pages/Login'
-import SignUp from './pages/SignUp'
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Customer from "./customer/Customer";
+import Admin from "./admin/Admin";
+import Navbar from "./components/Navbar";
+import Home from "./components/Home";
+import Login from "./pages/Login";
+import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
+import { useSelector } from "react-redux";
 
 function App() {
+  const { userdata } = useSelector((state) => state.user);
 
   return (
     <>
       <BrowserRouter>
-        <Navbar/>
+        <Navbar />
         <Routes>
-          <Route path='/' element={<Home />}/>
-          <Route path='/customer' element={<Customer/>}/>
-          <Route path='/admin' element={<Admin/>}/>
-          <Route path='/signup' element={<SignUp/>}/>
-          <Route path='/login' element={<Login />}/>
+          <Route path="/" element={<Home />} />
+          <Route
+            element={<PrivateRoute role="customer" userRole={userdata?.role} />}
+          >
+            <Route path="/customer" element={<Customer />} />
+          </Route>
+
+          <Route
+            element={<PrivateRoute role="admin" userRole={userdata?.role} />}
+          >
+            <Route path="/admin" element={<Admin />} />
+          </Route>
+
+          <Route path="/signup" element={<SignUp />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ client/src/components/PrivateRoute.jsx
@@ -0,0 +1,22 @@
+import { useSelector } from "react-redux";
+import { Navigate, Outlet } from "react-router-dom";
+
+//To keep the Customer & Admin routes private
+const PrivateRoute = ({ role, userRole }) => {
+  const { userdata } = useSelector((state) => state.user);
+
+  //Not loggedin then redirect to the Login page
+  if (!userdata) {
+    return <Navigate to="/login" />;
+  }
+
+  //Trying to access different role, redirects to Home page
+  if (userRole !== role) {
+    return <Navigate to="/" />;
+  }
+
+  //If user is logged in as well as roles are matching, relevant user data will be displayed.
+  return <Outlet />;
+};
+
+export default PrivateRoute;
