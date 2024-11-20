43306d06c6339c1506897cf09c6273a43f90a135
ESSARHTHUMAR <essarthumar177@gmail.com>
2024-11-20 12:37:04 +0530

    Vercel - Deploy

--- server/index.js
+++ server/index.js
@@ -33,7 +33,7 @@ app.use("/api", fileRouter);
 app.use(express.static(path.join(__dirname, '/client/dist')))
 
 app.get("*", (req,res) => {
-    res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'))
+    res.sendFile(path.resolve(__dirname, '../client/dist', 'index.html'));
 })
 
 //Handling errors using the Middleware
